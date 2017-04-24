9871c0778b09ccf03813ffb7a019deff0294fc3a
MIKE-RANDELL <michael.james.randell@gmail.com>
2017-04-24 13:02:31 -0400

    heroku db config 5

--- config/application.rb
+++ config/application.rb
@@ -12,13 +12,16 @@ module WilliamsRailsAngularApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.initialize_on_precompile = false
+    gz = GzipWriter.new(f)
+    gz.write 'jugemu jugemu gokou no surikire...'
+    gz.close
     # Bower asset paths
     root.join('vendor', 'assets', 'bower_components').to_s.tap do |bower_path|
       config.sass.load_paths << bower_path
       config.assets.paths << bower_path
     end
     # Precompile Bootstrap fonts
-    #config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
+    config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
     # Minimum Sass number precision required by bootstrap-sass
     ::Sass::Script::Value::Number.precision = [8, ::Sass::Script::Value::Number.precision].max
   end
